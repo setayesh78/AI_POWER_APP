ec9b3b5c8df23778595313541351dd9638b06315
setayesh78 <sanavisetayesh@gmail.com>
2025-11-07 17:07:24 -0500

    Set up prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -9,15 +9,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //define a route
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello, World');
+app.get('/', (req: Request, res: Response) => {
+   res.send('Hello, World');
 });
 
-app.get('/api/hello', (req:Request, res:Response) => {
-  res.json({message: 'Hello, World'});
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'Hello, World' });
 });
 
 //start the server
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+   console.log(`Server is running at http://localhost:${PORT}`);
 });
